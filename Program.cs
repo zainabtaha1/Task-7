0029970e6612f89a7c378aedf72f297dfa994849
zainabtaha1 <121588138+zainabtaha1@users.noreply.github.com>
2023-02-28 04:03:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Problem 6
+            double fahrenheit, Celsuis;
+            Console.WriteLine(" Enter the temprature in fahrenheit ");
+            fahrenheit = int.Parse(Console.ReadLine());
+            Celsuis = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine(" Temprature in celsuis is : " + Celsuis);
+        }
+    }
+}
